edf634b8ab3c805e6398fe31e02bf0a368026410
Elvine7067 <achiengelvine1@gmail.com>
2024-01-14 16:24:48 +0300

    Rock paper scissors

--- index9.py
+++ index9.py
@@ -3,6 +3,14 @@ value = input("please enter a value:\n")
 print(value)
 import sys
 import random
+from enum import Enum
+
+class RPS(Enum):
+    ROCK=1
+    PAPER=2
+    SCISSORS=3
+print(RPS(2))
+
 print('')
 playerchoice = input("Enter...\n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n" )
 player = int(playerchoice)
@@ -18,12 +26,12 @@ print("you chose" + playerchoice +".")
 print("python chose" + computerchoice + ".")
 print("")
 if player == 1:
-    print("You win!")
+    print("🎉You win!")
 elif player == 2 and computer ==1:
-    print("You win!")
+    print("🎉You win!")
 elif player == 3 and computer == 2:
-    print("You win!")
+    print("🎉You win!")
 elif player ==  computer:
-    print("Tie game!")    
+    print("😮Tie game!")    
 else:
-    print("python wins!")
+    print("🐍python wins!")
